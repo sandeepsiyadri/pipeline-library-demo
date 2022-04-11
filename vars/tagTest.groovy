21784efcb4e750061125d9e8c4f9369d62addfa8
ssiyadri <ssiyadri07@gmail.com>
2022-04-10 20:24:33 -0400

    updated generic pipeline

--- vars/tagTest.groovy
+++ vars/tagTest.groovy
@@ -2,4 +2,161 @@
 
 def call(String name = 'Sandeep Siyadri') {
   echo "Hello, ${name}."
+
+  if("${env.BRANCH_NAME}".contains('feature')) {
+    pipeline {
+
+        agent any
+
+        options {
+            buildDiscarder logRotator(
+                        daysToKeepStr: '16',
+                        numToKeepStr: '10'
+                )
+        }
+
+        stages {
+
+            stage('Cleanup Workspace') {
+                steps {
+                    cleanWs()
+                    sh """
+                    echo "Cleaned Up Workspace For Project"
+                    """
+                }
+            }
+
+            stage('Branch Checkout') {
+                when {
+                        expression{env.BRANCH_NAME == 'master'||'develop'||'feature'}
+                }
+                steps {
+                        checkout([
+                        $class: 'GitSCM',
+                            branches: [[name: "${env.BRANCH_NAME}"]],
+                            userRemoteConfigs: [[credentialsId: 'githubcred', url: 'https://github.com/sandeepsiyadri/multibranch-pipeline-demo.git']]
+                        ])
+                }
+            }
+            /* stage('Tag Checkout') {
+                when {
+                      tag "release-*"
+                }
+                steps {
+                        script {
+                            TAG_VERSION = sh(returnStdout: true, script: "git tag --contains").trim()
+                            echo "tag version is ${TAG_VERSION}"
+                        }
+                        checkout([
+                            $class: 'GitSCM',
+                            branches: [[name: "refs/tags/${TAG_VERSION}"]],
+                            userRemoteConfigs: [[credentialsId: 'githubcred', url: 'https://github.com/sandeepsiyadri/multibranch-pipeline-demo.git']]
+                        ])
+                }
+            } */
+
+            stage(' Unit Testing') {
+                steps {
+                    sh """
+                    echo "Running Unit Tests"
+                    """
+                }
+            }
+
+            stage('Code Analysis') {
+                steps {
+                    sh """
+                    echo "Running Code Analysis"
+                    """
+                }
+            }
+
+            stage('Build Deploy Code') {
+                when {
+                    branch 'develop'
+                }
+                steps {
+                    sh """
+                    echo "Building Artifact"
+                    """
+
+                    sh """
+                    echo "Deploying Code"
+                    """
+                }
+            }
+
+        }
+    }
+  } else if ("${env.TAG_NAME}".contains('v')) {
+    pipeline {
+
+        agent any
+
+        options {
+            buildDiscarder logRotator(
+                        daysToKeepStr: '16',
+                        numToKeepStr: '10'
+                )
+        }
+
+        stages {
+
+            stage('Cleanup Workspace') {
+                steps {
+                    cleanWs()
+                    sh """
+                    echo "Cleaned Up Workspace For Project"
+                    """
+                }
+            }
+            stage('Tag Checkout') {
+                steps {
+                        script {
+                            TAG_VERSION = sh(returnStdout: true, script: "git tag --contains").trim()
+                            echo "tag version is ${TAG_VERSION}"
+                        }
+                        checkout([
+                            $class: 'GitSCM',
+                            branches: [[name: "refs/tags/${TAG_VERSION}"]],
+                            userRemoteConfigs: [[credentialsId: 'githubcred', url: 'https://github.com/sandeepsiyadri/multibranch-pipeline-demo.git']]
+                        ])
+                }
+            }
+
+            stage(' Unit Testing') {
+                steps {
+                    sh """
+                    echo "Running Unit Tests TAG"
+                    """
+                }
+            }
+
+            stage('Code Analysis') {
+                steps {
+                    sh """
+                    echo "Running Code Analysis TAG"
+                    """
+                }
+            }
+
+            stage('Build Deploy Code') {
+                when {
+                    branch 'develop'
+                }
+                steps {
+                    sh """
+                    echo "Building Artifact TAG"
+                    """
+
+                    sh """
+                    echo "Deploying Code TAG"
+                    """
+                }
+            }
+
+        }
+    }
+  }
+
 }
